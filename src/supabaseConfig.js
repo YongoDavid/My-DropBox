6f82545ddcd15fb3056ee51c15e29e3ed87ce416
Yongo David <davidyongo9@gmail.com>
2025-01-10 06:11:53 +0100

    evn file commit

--- src/supabaseConfig.js
+++ src/supabaseConfig.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://dvfkypgoadzkjukinmbn.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2Zmt5cGdvYWR6a2p1a2lubWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUxODQzMzcsImV4cCI6MjA1MDc2MDMzN30.ouEJUUnOIDU01CQi6huOFjedgGnoeShkh2yEh4kzs5I";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
