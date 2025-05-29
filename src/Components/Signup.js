2f435da0c120df063bfa77e1a5c2b45bb2eedb9b
Yongo David <davidyongo9@gmail.com>
2025-05-29 21:21:46 +0100

    Signup page update

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -91,7 +91,7 @@ export default function Signup() {
               Create Account
             </Heading>
             <Text fontSize="lg" color="gray.600">
-              Save and access your files from any device, and share them with anyone. Try Storebox today.
+              Save and access your files from any device,Try Storebox today.
             </Text>
           </VStack>
 
